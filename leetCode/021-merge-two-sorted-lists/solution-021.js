6ad48146c0a46c70e142389b7da858473aea0369
Jonathan J. Pak <jpakdev@gmailcom>
2025-04-09 10:42:58 -0700

    lc021: Wrote out solution for merge two linked lists

--- leetCode/021-merge-two-sorted-lists/solution-021.js
+++ leetCode/021-merge-two-sorted-lists/solution-021.js
@@ -1,10 +1,51 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+class LinkedList {
+    constructor(headNode) {
+        if(!headNode) {
+            this.head = null;
+        } else {
+            this.head = headNode;
+        }
+    }
+  
+    // Add a node to the end
+    append(value) {
+      const newNode = new ListNode(value);
+  
+      if (!this.head) {
+        this.head = newNode;
+        return;
+      }
+  
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+  
+      current.next = newNode;
+    }
+
+    getArray() {
+        let current = this.head;
+        let array = [];
+        while (current) {
+            array.push(current.val);
+            current = current.next;
+        }
+        return array;
+    }
+  
+    // Print all values (for testing)
+    print() {
+      console.log(this.getArray());
+    }
+}
+
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
@@ -12,21 +53,40 @@
  */
 var mergeTwoLists = function(list1, list2) {
     // Create a dummy list to populate
-
+    const dummy = new ListNode();
+    let current = dummy;
 
     // Use a while loop to iterate through list1 and list2
     // Stop list when both list values are null
+    while (list1 && list2) {
+        // Compare values
+        if(list1.val < list2.val) {
+            // Insert lower value into dummy list
+            current.next = list1;
+            // Reassign next for the list that's value was used and dummy list
+            list1 = list1.next;
+        } else {
+            // Insert lower value into dummy list
+            current.next = list2;
+            // Reassign next for the list that's value was used and dummy list
+            list2 = list2.next;
+        }
+        // Reassign next for the list that's value was used and dummy list
+        current = current.next;
+    }
 
-    // Compare values
+    // Handle when only one of the lists is empty
+    if (list1 && !list2) {
+        current.next = list1;
+    } else if (list2 && !list1) {
+        current.next = list2;
+    }
 
-    // Insert lower value into dummy list
-
-    // Reassign next for the list that's value was used and dummy list
-
-    // Return dummy list head
+    // Return next because initialized dummy.val is 0
+    return dummy.next;
 };
 
-export {mergeTwoLists};
+export {mergeTwoLists, ListNode, LinkedList};
 
 
 
--- leetCode/021-merge-two-sorted-lists/testCases-021.js
+++ leetCode/021-merge-two-sorted-lists/testCases-021.js
@@ -1,20 +1,44 @@
-import { mergeTwoLists } from "./solution-021";
+import { mergeTwoLists, ListNode, LinkedList } from "./solution-021.js";
+
+const l1 = new LinkedList();
+l1.append(1);
+l1.append(2);
+l1.append(4);
+const l2 = new LinkedList();
+l2.append(1);
+l2.append(3);
+l2.append(4);
 
 const case1 = {
-    list1: [1, 2, 4],
-    list2: [1, 3, 4]
+    list1: l1,
+    list2: l2
 };
+
+const l3 = new LinkedList();
+const l4 = new LinkedList();
 
 const case2 = {
-    list1: [],
-    list2: []
+    list1: l3,
+    list2: l4
 };
+
+const l5 = new LinkedList();
+const l6 = new LinkedList();
+l6.append(0);
 
 const case3 = {
-    list1: [],
-    list2: [0]
+    list1: l5,
+    list2: l6
 };
 
-console.log(`Expected: [1, 1, 2, 3, 4, 4]\n Actual:${mergeTwoLists(case1.list1, case1.list2)}`);
-console.log(`Expected: []\nActual:${mergeTwoLists(case2.list1, case1.list2)}`);
-console.log(`Expected: [0]\nActual:${mergeTwoLists(case3.list1, case1.list2)}`);
+const m1 = new LinkedList(mergeTwoLists(case1.list1.head, case1.list2.head));
+console.log(`Case 1 Expected:\n[ 1, 1, 2, 3, 4, 4 ]\nCase 1 Actual:`);
+m1.print();
+
+const m2 = new LinkedList(mergeTwoLists(case2.list1.head, case2.list2.head));
+console.log(`Case 2 Expected:\n[]\nCase 2 Actual:`);
+m2.print();
+
+const m3 = new LinkedList(mergeTwoLists(case3.list1.head, case3.list2.head));
+console.log(`Case 3 Expected:\n[ 0 ]\nCase 3 Actual:`);
+m3.print();
